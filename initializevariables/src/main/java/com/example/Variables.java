fcf76fc1ea617c9befef834e65ec72eb667ec290
TheLostKing <calderonezj@gmail.com>
2015-09-01 12:55:36 -0500

    Initial commit

---
+++ initializevariables/src/main/java/com/example/Variables.java
@@ -0,0 +1,22 @@
+/* Zachary Calderone
+   This is my homework due on September 7th for Java 1
+ */
+package com.example;
+
+public class Variables
+{
+    public static void main(String[] args)
+    {
+        byte B = 1;
+        short S = 3;
+        int I = 69;
+        long L = 1337666;
+        float F = 6.9f;
+        double D = 3.14;
+        boolean Peace = false;
+        char C = 'z';
+
+        System.out.println("Byte: " + B + "\nShort: " + S + "\nInt: " + I + "\nLong: " + L + "\nFloat: " + F + "\nDouble: " + D + "\nBoolean: " + Peace + "\nChar: " + C);
+
+    }
+}
